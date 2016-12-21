ac7e7e3d0c707a216b8ac9e671dc7403b3fd6a4e
Rene Sorel <rene@proversity.org>
2016-12-21 15:33:05 +0200

    createadmin should be a management command

--- apps/mainsite/management/commands/createadmin.py
+++ apps/mainsite/management/commands/createadmin.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
 
 import os
+from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
+class Command(BaseCommand):
+    args = ''
+    help = 'Creates superuser'
 
-username = os.environ.get('ADMIN_USERNAME')
-password = os.environ.get('ADMIN_PASSWORD')
+    def handle(self, *args, **options):
+        """
+        Creates superuser if environment variables are set
+        """
+        User = get_user_model()
 
-if username and password:
-  if User.objects.count() == 0:
-      admin = User.objects.create(username=username)
-      admin.set_password(password)
-      admin.is_superuser = True
-      admin.is_staff = True
-      admin.save()
+        username = os.environ.get('ADMIN_USERNAME')
+        password = os.environ.get('ADMIN_PASSWORD')
+
+        if username and password:
+          if User.objects.count() == 0:
+              admin = User.objects.create(username=username)
+              admin.set_password(password)
+              admin.is_superuser = True
+              admin.is_staff = True
+              admin.save()
+
+
