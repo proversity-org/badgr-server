a8ef98419824c06445c6c25448821fc1d79fa5f6
Rene Sorel <rene@proversity.org>
2016-12-22 13:20:34 +0200

    try wsgi for django 1.7

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
+# Django <= 1.7
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+# Django >= 1.8
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
