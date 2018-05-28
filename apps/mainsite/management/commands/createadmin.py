2ef8f101cc179e81b89bee70c8631fc6a534f7b9
nicovanniekerk <nico@proversity.org>
2018-05-28 12:17:28 +0200

    ADD: admin user create management command

---
+++ apps/mainsite/management/commands/createadmin.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Creates superuser'
+
+    def handle(self, *args, **options):
+        """
+        Creates superuser if environment variables are set
+        """
+        User = get_user_model()
+
+        username = os.environ.get('ADMIN_USERNAME')
+        password = os.environ.get('ADMIN_PASSWORD')
+
+        if username and password:
+          if User.objects.count() == 0:
+              admin = User.objects.create(username=username)
+              admin.set_password(password)
+              admin.is_superuser = True
+              admin.is_staff = True
+              admin.save()
